9ff023c743de3a975095b5fc8dee6dde5318c8e8
lauro.gama <lauro.gama@fpf.br>
2016-05-12 17:04:53 -0400

    suporte a sqlAlchemy, Material Design, assets

---
+++ server/assets.py
@@ -0,0 +1,15 @@
+from flask_assets import Bundle
+
+bundles = {
+
+    'js': Bundle(
+        # 'static/js/jquery-1.10.2.js',
+        'js/material.min.js',
+        # 'js/crudMonitores.js'
+    ),
+
+    'css': Bundle(
+        'css/material.min.css',
+        # 'css/style.css'
+    )
+}
--- server/server.py
+++ server/server.py
@@ -1,6 +1,19 @@
+import logging
+
 from flask import Flask
+from flask.ext.assets import Environment
+from flask_sqlalchemy import SQLAlchemy
+
+from assets import bundles
+from settings import CONFIG
 
 app = Flask(__name__)
+assets = Environment(app)
+assets.register(bundles)
+configuration = CONFIG
+app.config['SECRET_KEY'] = configuration['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = configuration['SQLITE_TEST_DB']
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -9,4 +22,6 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    logging.basicConfig(filename=configuration['LOG_PATH'], level=logging.INFO, debug=True)
+    db.create_all()
+    app.run(host='0.0.0.0', port=configuration['APP_PORT'])
